7d7f2305e19bd50ebe16dcbc0998bc3a8e22ec8b
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 07:56:04 -0500

    update - route 'reset-password'

--- src/routes/reset-password.svelte
+++ src/routes/reset-password.svelte
@@ -1,27 +1,36 @@
 <script>
+  // import
+  import { goto } from '$app/navigation';
+
   // components
-  import { Button, Input, Link } from '$components';
+  import { Button, Input, Layout, Link } from '$components';
 
   // handlers
-  const submitHandler = () => {}
+  const submitHandler = () => {
+    goto('/reset-password-confirmation');
+  };
 
   // props (internal)
   let email = '';
 </script>
 
-<form
-  on:submit|preventDefault={submitHandler}
-  class="flex flex-col space-y-[2rem] px-[1rem] w-full"
->
-  <div class="flex flex-col space-y-[1rem] items-center">
-    <div class="text-center font-semibold text-[1.3125rem]">Forgot your password?</div>
-    <div class="text-center max-w-[250px] opacity-[.5] text-[.875rem]">Enter your email below to receive password reset instructions</div>
-  </div>
-  <Input name="email" placeholder="Enter email address" type="email" bind:value={email} />
-  <Button>Reset Password</Button>
-  <div class="flex space-x-[.5rem] justify-center">
-    <div>Remember password?</div>
-    <Link href="./sign-in">Sign In</Link>
-  </div>
-  <slot/>
-</form>
+<Layout>
+  <form
+    on:submit|preventDefault={submitHandler}
+    class="flex flex-col space-y-[2rem] px-[1rem] w-full"
+  >
+    <div class="flex flex-col space-y-[1rem] items-center">
+      <div class="text-center font-semibold text-[1.5rem]">Forgot your password?</div>
+      <div class="text-center max-w-[21.4375rem] opacity-[.5]">
+        Enter your email below to receive password reset instructions
+      </div>
+    </div>
+    <Input name="email" placeholder="Enter email address" type="email" bind:value={email} />
+    <Button type="submit">Reset Password</Button>
+    <div class="flex space-x-[.5rem] justify-center">
+      <div>Remember password?</div>
+      <Link href="./sign-in">Sign In</Link>
+    </div>
+    <slot />
+  </form>
+</Layout>
