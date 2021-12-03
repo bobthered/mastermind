46e2c70143c718bd6f1f267644483978eab3e7c2
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 08:03:28 -0500

    add - miscellaneous packages

--- src/socketio/index.js
+++
@@ -1,1 +0,0 @@
-export { default as socketEvents } from './socketEvents.js';
--- src/socketio/socketEvents.js
+++
@@ -1,9 +0,0 @@
-// imports
-import * as events from './events/index.js';
-
-export default (io, socket) => {
-  console.log('socket.io - connection');
-  Object.values(events).forEach(event => {
-    event(io, socket);
-  })
-}
