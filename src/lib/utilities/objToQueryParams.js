29fd51fd1f38993d7215e4ee8433857e48e42698
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 08:40:26 -0500

    fix - use spread operator to return reduced array

--- src/lib/utilities/objToQueryParams.js
+++ src/lib/utilities/objToQueryParams.js
@@ -1,1 +1,1 @@
-export default obj => Object.keys(obj).reduce((array, key) => { array.push(`${key}=${obj[key]}`) }, []).join('&')
+export default obj => Object.keys(obj).reduce((array, key) => [...array, `${key}=${obj[key]}`], []).join('&')
