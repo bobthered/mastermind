22efc0ff993a061531349f25d61d400dfe71cbed
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 07:51:13 -0500

    add - component 'Scrollable'

---
+++ src/components/Scrollable/Default.svelte
@@ -0,0 +1,14 @@
+<script>
+  // props (external)
+  export let direction = 'horizontal';
+  export let style = '';
+
+  // props (dynamic)
+  $: classes = direction === 'horizontal' ? 'overflow-x-auto' : 
+               direction === 'vertical' ? 'overflow-y-auto' :
+               direction === 'both' ? 'overflow-auto' : ''
+</script>
+
+<div class="{classes} {$$props.class !== undefined ? $$props.class : ''}" {style}>
+  <slot/>
+</div>
