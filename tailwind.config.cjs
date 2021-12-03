547cba62513f66bc3bee747a0cc289e3ea2ce37b
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 08:01:58 -0500

    update - add orange color & aspec-ratio plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,23 @@
+const colors = require('tailwindcss/colors');
+
 const config = {
   mode: 'jit',
   purge: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
     extend: {
+      colors: {
+        orange: colors.orange
+      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif']
       }
     }
   },
 
-  plugins: []
+  plugins: [
+    require('@tailwindcss/aspect-ratio')
+  ]
 };
 
 module.exports = config;
