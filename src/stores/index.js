c0274fcd30a65fb471e29982c8a59cc64c8fc7cd
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 07:59:38 -0500

    update - store module

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,2 +1,4 @@
+export { default as auth } from './auth.js';
+export { default as modal } from './modal.js';
 export { default as socket } from './socket.js';
 export { default as theme } from './theme.js';
