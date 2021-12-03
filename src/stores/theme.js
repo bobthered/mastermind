92ee867d4cff427e8a19c98c6641f6128d852019
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 07:59:02 -0500

    update - store 'theme'

--- src/stores/theme.js
+++ src/stores/theme.js
@@ -4,17 +4,116 @@ import { writable } from 'svelte/store';
 // initiate default store
 const defaults = {
   bgColor: 'bg-gray-900',
-  textColor: 'text-white',
+  contrastBgColor: 'bg-white',
+  contrastTextColor: 'text-gray-900',
+  color: 'blue',
+  darkMode: true,
+  fontSize: 16,
   primaryBgColor: 'bg-blue-500',
   primaryContrastTextColor: 'text-white',
   primaryRingColor: 'ring-blue-500',
   primaryTextColor: 'text-blue-500',
+  textColor: 'text-white',
 }
 
 const createStore = () => {
-  const { set, subscribe, update } = writable(defaults);
+  const store = writable(defaults);
 
-  return { set, subscribe, update }
+  // methods
+  const reset = () => {
+    store.update(() => {
+      return {
+        bgColor: 'bg-gray-900',
+        contrastBgColor: 'bg-white',
+        contrastTextColor: 'text-gray-900',
+        color: 'blue',
+        darkMode: true,
+        fontSize: 16,
+        primaryBgColor: 'bg-blue-500',
+        primaryContrastTextColor: 'text-white',
+        primaryRingColor: 'ring-blue-500',
+        primaryTextColor: 'text-blue-500',
+        textColor: 'text-white',
+      }
+    });
+  }
+  const set = ({ color, darkMode, fontSize }) => {
+    store.update(state => updateState({ color, darkMode, fontSize, state }))
+  }
+  const update = ({ color, darkMode, fontSize }) => {
+    store.update(state => updateState({ color, darkMode, fontSize, state }))
+  }
+  const updateState = ({ color, darkMode, fontSize, state }) => {
+    // update color
+    state.color = color;
+
+    // update darkMode
+    state.darkMode = darkMode;
+
+    // update fontSize
+    state.fontSize = fontSize;
+
+    // update color settings
+    if (color === 'pink') {
+      state.primaryBgColor = 'bg-pink-500';
+      state.primaryRingColor = 'ring-pink-500';
+      state.primaryTextColor = 'text-pink-500';
+      state.primaryContrastTextColor = 'text-white';
+    }
+    if (color === 'red') {
+      state.primaryBgColor = 'bg-red-500';
+      state.primaryRingColor = 'ring-red-500';
+      state.primaryTextColor = 'text-red-500';
+      state.primaryContrastTextColor = 'text-white';
+    }
+    if (color === 'orange') {
+      state.primaryBgColor = 'bg-orange-500';
+      state.primaryRingColor = 'ring-orange-500';
+      state.primaryTextColor = 'text-orange-500';
+      state.primaryContrastTextColor = 'text-white';
+    }
+    if (color === 'yellow') {
+      state.primaryBgColor = 'bg-yellow-500';
+      state.primaryRingColor = 'ring-yellow-500';
+      state.primaryTextColor = 'text-yellow-500';
+      state.primaryContrastTextColor = 'text-gray-900';
+    }
+    if (color === 'green') {
+      state.primaryBgColor = 'bg-green-500';
+      state.primaryRingColor = 'ring-green-500';
+      state.primaryTextColor = 'text-green-500';
+      state.primaryContrastTextColor = 'text-white';
+    }
+    if (color === 'blue') {
+      state.primaryBgColor = 'bg-blue-500';
+      state.primaryRingColor = 'ring-blue-500';
+      state.primaryTextColor = 'text-blue-500';
+      state.primaryContrastTextColor = 'text-white';
+    }
+    if (color === 'purple') {
+      state.primaryBgColor = 'bg-purple-500';
+      state.primaryRingColor = 'ring-purple-500';
+      state.primaryTextColor = 'text-purple-500';
+      state.primaryContrastTextColor = 'text-white';
+    }
+
+    // update darkMode settings
+    if (darkMode) {
+      state.bgColor = 'bg-gray-900';
+      state.contrastBgColor = 'bg-white';
+      state.contrastTextColor = 'text-gray-900';
+      state.textColor = 'text-white';
+    }
+    if (!darkMode) {
+      state.bgColor = 'bg-gray-50';
+      state.contrastBgColor = 'bg-gray-900';
+      state.contrastTextColor = 'text-white';
+      state.textColor = 'text-gray-900';
+    }
+    return state;
+  }
+
+  return { reset, set, subscribe: store.subscribe, update }
 }
 
 export default createStore();
