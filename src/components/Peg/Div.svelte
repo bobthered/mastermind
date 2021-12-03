46f3d0bebf58a0163a1db038da1506f9ff129288
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 14:28:00 -0500

    refactor - centralize class generation

--- src/components/Peg/Div.svelte
+++ src/components/Peg/Div.svelte
@@ -1,24 +1,18 @@
 <script>
   // imports
   import { theme } from '$stores';
-  
+  import { generateClasses } from './index.js';
+
   // props (external)
   export let color = 'blue';
+  export let current = false;
 
   // props (dynamic)
-  $: colorClasses = color === 'pink' ?    'bg-pink-500 text-white' :
-                    color === 'red' ?     'bg-red-500 text-white' :
-                    color === 'orange' ?  'bg-orange-500 text-white' :
-                    color === 'yellow' ?  'bg-yellow-500 text-gray-900' :
-                    color === 'green' ?   'bg-green-500 text-white' :
-                    color === 'blue' ?    'bg-blue-500 text-white' :
-                    color === 'purple' ?  'bg-purple-500 text-white' :
-                    color === 'contrast' ? $theme.contrastBgColor : 
-                                           `ring-1 ring-opacity-[.1] ${$theme.ringColor}`
+  $: classes = generateClasses(color, $theme, current);
 </script>
 
 <div
-  class="w-[3rem] h-[3rem] flex items-center justify-center rounded-full {colorClasses}"
+  class={classes}
 >
   <slot/>
 </div>
--- src/components/Peg/index.js
+++ src/components/Peg/index.js
@@ -1,3 +1,17 @@
+export const defaultClasses = 'w-[3rem] h-[3rem] flex items-center justify-center rounded-full transition duration-200 ring-4';
+export const generateClasses = (color, theme, current = false) => {
+  const colorClasses = color === 'pink' ?    `bg-pink-500 text-white ring-pink-500`:
+                       color === 'red' ?     `bg-red-500 text-white ring-red-500` :
+                       color === 'orange' ?  `bg-orange-500 text-white ring-orange-500`:
+                       color === 'yellow' ?  `bg-yellow-500 text-gray-900 ring-yellow-500`:
+                       color === 'green' ?   `bg-green-500 text-white ring-green-500`:
+                       color === 'blue' ?    `bg-blue-500 text-white ring-blue-500`:
+                       color === 'purple' ?  `bg-purple-500 text-white ring-purple-500`:
+                       color === 'contrast' ? `${theme.contrastBgColor} ${theme.contrastTextColor} ${theme.ringColor}` : 
+                                             `ring-1 ring-opacity-[.1] ${theme.ringColor}`
+  return `${defaultClasses} ${colorClasses} ${current ? 'ring-opacity-[.3]' : 'ring-opacity-0'}`
+}
+
 export { default as default } from './Default.svelte';
 export { default as Button } from './Button.svelte';
 export { default as Div } from './Div.svelte';
