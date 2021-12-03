8156e558b92d47d3c31bee739f7773c142a6d4d0
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-03 07:49:23 -0500

    add - component 'InputContainer'

---
+++ src/components/InputContainer/Default.svelte
@@ -0,0 +1,9 @@
+<script>
+  // props (external)
+  export let label = '';
+</script>
+
+<div class="space-y-[1rem] {$$props.class !== undefined ? $$props.class : ''}">
+  <div class="font-medium">{label}</div>
+  <slot/>
+</div>
