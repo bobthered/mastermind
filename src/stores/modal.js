de5164e7b6eed246ce661b1487404d38a025c2d3
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-06 07:17:39 -0500

    add - modals 'Lost' and 'Win'

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,7 +1,11 @@
 import { store as error } from '$components/Modal/Error';
+import { store as lost } from '$components/Modal/Lost';
 import { store as spinner } from '$components/Modal/Spinner';
+import { store as win } from '$components/Modal/Win';
 
-export default { 
+export default {
   error,
+  lost,
   spinner,
+  win,
 }
