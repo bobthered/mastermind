6de21a440e7507cc45a8380f604ccaa053777ea1
Bob McAleavey <bobmcaleavey@gmail.com>
2021-12-08 12:30:57 -0500

    add - store 'game'

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,5 @@
 export { default as auth } from './auth.js';
+export { default as game } from './game.js';
 export { default as modal } from './modal.js';
 export { default as socket } from './socket.js';
 export { default as theme } from './theme.js';
